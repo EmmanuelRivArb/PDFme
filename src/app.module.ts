19d34d90b22e476a508b0a49f188ca78b1205949
Emmanuel Rivero <enmanuelrivero@itop.es>
2023-05-26 08:50:53 +0100

    feat: add TypeORM, GrapQL and Config modules to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DocumentModule } from './document/document.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
-  imports: [DocumentModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      includeStacktraceInErrorResponses: false,
+      cors: {
+        origin: '*',
+        credentials: true,
+      },
+    }),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    DocumentModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
